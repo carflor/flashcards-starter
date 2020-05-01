8b9dadc53c759203fcc5a9390dab81e8c925dab1
carflor <cfdeclet@gmail.com>
2020-05-01 17:56:09 -0600

    Removed comments and removed card require bug

--- src/Game.js
+++ src/Game.js
@@ -1,35 +1,12 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-const Card = require('..src/Card');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 
 class Game {
   constructor() {
-    // Should keep track of the currentRound
     this.currentRound = 0;
-  }
-
-  // start method
-  startGame() {
-    // creates cards
-    const allCards = prototypeQuestions.map(cardData => {
-      let cards = new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer)
-      return cards
-    })
-    // places cards in deck
-    let deck = new Deck([cards])
-    // creates a new round using deck
-    let round = new Round(deck)
-    // invokes printMessage to display message
-    this.printMessage(deck, round)
-    // invokes questions to kick off interaction
-    this.printQuestions(round)
-    // also keeps track of current found property
-    this.currentRound++
-    
-
   }
 
   printMessage(deck, round) {
@@ -39,6 +16,18 @@ class Game {
 
   printQuestion(round) {
     util.main(round);
+  }
+
+  start() {
+    const allCards = prototypeQuestions.map(cardData => {
+      let cards = new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer)
+      return cards
+    })
+    let deck = new Deck(allCards)
+    let round = new Round(deck)
+    this.printMessage(deck, round)
+    this.printQuestions(round)
+    this.currentRound++
   }
 }
 
